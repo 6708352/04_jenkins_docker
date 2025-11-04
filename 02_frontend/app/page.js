e71e4b73a79d101de32298829f6bffe3145d7a3a
6708352 <kelseylekzeen.d67@rsu.ac.th>
2025-11-04 17:53:12 +0700

    Add my name

--- 02_frontend/app/page.js
+++ 02_frontend/app/page.js
@@ -44,6 +44,7 @@ export default function Page() {
   return (
     <main className="container">
       <header className="header">
+        <h1 className="title">Kelsey L Dorji 6708352</h1>
         <h1 className="title">Attractions</h1>
         <p className="subtitle">Discover points of interest nearby</p>
       </header>
